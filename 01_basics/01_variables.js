9e7f9e3e16b946627a81e603babcfa43fcbab9f1
Vanyaa27 <151496725+Vanyaa27@users.noreply.github.com>
2024-03-02 09:44:16 +0000

    Intially

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144255
+let accountEmail = "vanya@google.com"
+var accountPassword = "v1v1v1"
+accountCity = "Delhi"
+let accountState;
+
+accountEmail= "vc@vc.com"
+accountPassword= "1818"
+accountCity="Ghaziabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
